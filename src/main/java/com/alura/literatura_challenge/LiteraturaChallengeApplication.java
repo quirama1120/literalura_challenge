d1c6ca1cc737694a4a1e2c3ee58fa19deb9f03fa
Christian Quirama <quirama1120@gmail.com>
2024-11-09 14:39:21 -0500

    creating git hub literalura_challenge path

---
+++ src/main/java/com/alura/literatura_challenge/LiteraturaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literatura_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/literatura_challenge/LiteraturaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literatura_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
